e7b774d8b6ef3ca1b4a9cfb2c51cc8071bc878f3
JohannGlsr <122325236+JohannGlsr@users.noreply.github.com>
2023-05-18 10:57:29 +0200

    Add files via upload

---
+++ APP-p8.py
@@ -0,0 +1,22 @@
+import requests
+import streamlit as st
+from PIL import Image
+from io import BytesIO
+
+def main():
+    st.title("Application de segmentation d'images")
+
+    image_file = st.file_uploader("Sélectionnez une image", type=['jpg', 'jpeg', 'png'])
+    if image_file is not None:
+        files = {'image': image_file}
+        response = requests.post('http://127.0.0.1:5000/predict', files=files)
+        if response.status_code == 200:
+            st.success('Image segmentée avec succès.')
+            flipped_image_bytes = response.content
+            flipped_image = Image.open(BytesIO(flipped_image_bytes))
+            st.image(flipped_image, use_column_width=True)
+        else:
+            st.error('Une erreur s\'est produite lors de la segmentation de l\'image.')
+
+if __name__ == '__main__':
+    main()
